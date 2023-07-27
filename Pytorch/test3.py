0ae1c8227a4a0084e79ea5a9e4ba3050cee615a3
Debjit Pal <debjitpal5040@gmail.com>
2023-07-28 00:30:47 +0530

    gg

--- Pytorch/test3.py
+++ Pytorch/test3.py
@@ -1,5 +1,7 @@
-import torch
 import math
+import torch
+
+
 dtype = torch.float
 device = torch.device("mps")
 
@@ -16,19 +18,19 @@ d = torch.randn((), device=device, dtype=dtype)
 learning_rate = 1e-6
 for t in range(2000):
     # Forward pass: compute predicted y
-    y_pred = a + b * x + c * x ** 2 + d * x ** 3
+    y_pred = a + b * x + c * x**2 + d * x**3
 
     # Compute and print loss
     loss = (y_pred - y).pow(2).sum().item()
     if t % 100 == 99:
         print(t, loss)
 
-# Backprop to compute gradients of a, b, c, d with respect to loss
+    # Backprop to compute gradients of a, b, c, d with respect to loss
     grad_y_pred = 2.0 * (y_pred - y)
     grad_a = grad_y_pred.sum()
     grad_b = (grad_y_pred * x).sum()
-    grad_c = (grad_y_pred * x ** 2).sum()
-    grad_d = (grad_y_pred * x ** 3).sum()
+    grad_c = (grad_y_pred * x**2).sum()
+    grad_d = (grad_y_pred * x**3).sum()
 
     # Update weights using gradient descent
     a -= learning_rate * grad_a
@@ -37,4 +39,4 @@ for t in range(2000):
     d -= learning_rate * grad_d
 
 
-print(f'Result: y = {a.item()} + {b.item()} x + {c.item()} x^2 + {d.item()} x^3')
+print(f"Result: y = {a.item()} + {b.item()} x + {c.item()} x^2 + {d.item()} x^3")
